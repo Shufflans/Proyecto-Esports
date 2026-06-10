a7ebcb4664872e4efab3dcd658028a1e9d7ab8d8
Camilo Covarrubias <camilocovarrubias18@gmail.com>
2026-06-10 02:25:19 -0400

    Corregida migración en MS-AUTH

--- ms-auth/src/main/resources/db/migration/V1__init.sql
+++ ms-auth/src/main/resources/db/migration/V1__init.sql
@@ -1,16 +1,16 @@
-CREATE TABLE usuarios (
-    id BIGINIT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE usuario (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE refresh_token (
-    id BIGNIT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     token VARCHAR(255) NOT NULL,
     username VARCHAR(55) NOT NULL,
     expiry_date DATETIME NOT NULL
-)
+);
 
 INSERT INTO usuario (username, password, role) VALUES (
     'admin',
@@ -20,4 +20,4 @@ INSERT INTO usuario (username, password, role) VALUES (
     'usuario',
     '$2a$12$Ej00Il93ygEci4JPlDcmyO.tM5llgEVyXHfG1gL0TBEyI4FsnSxla',
     'ROLE_USER'
-)
+);
