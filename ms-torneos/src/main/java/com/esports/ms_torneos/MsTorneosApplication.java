530d4875c9faafa1e56c74b29ffb63ba5bc90c61
Camilo Covarrubias <camilocovarrubias18@gmail.com>
2026-06-16 17:15:38 -0400

    Creado DTO, Model, Repository y DB

---
+++ ms-torneos/src/main/java/com/esports/ms_torneos/MsTorneosApplication.java
@@ -0,0 +1,13 @@
+package com.esports.ms_torneos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTorneosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTorneosApplication.class, args);
+	}
+
+}
