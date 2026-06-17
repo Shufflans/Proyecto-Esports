2bb8d0d5784e767ebfaa59c269253590118582b0
Camilo Covarrubias <camilocovarrubias18@gmail.com>
2026-06-17 19:58:35 -0400

    Cambio para inciar TEST

--- ms-transferencias/src/test/java/com/esports/ms_transferencias/MsTransferenciasApplicationTests.java
+++ ms-transferencias/src/test/java/com/esports/ms_transferencias/MsTransferenciasApplicationTests.java
@@ -2,10 +2,12 @@ package com.esports.ms_transferencias;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MsTransferenciasApplicationTests {
 
 	@MockitoBean
